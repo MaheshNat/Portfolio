ebadcad163840ebfb7003d9e38110cc94057908c
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-08-19 16:24:33 -0500

    added styling for resume pdf viewer

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 
@@ -11,83 +11,119 @@ import {
 
 import ReactGa from 'react-ga';
 
-export default function Resume() {
-  const [pages, setPages] = useState(null);
-  const [page, setPage] = useState(1);
-  return (
-    <div className="container">
-      <div
-        className="jumbotron text-center"
-        style={{ marginBottom: '2em', marginTop: '2em' }}
-      >
-        <h1>
-          Resume{' '}
-          <a
-            href={'http://mnat.herokuapp.com/api/resume'}
+export default class Resume extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pages: null,
+      page: 1,
+      width: null,
+      height: null,
+    };
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
+  updateDimensions() {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div
+          className="jumbotron text-center"
+          style={{ marginBottom: '2em', marginTop: '2em' }}
+        >
+          <h1>
+            Resume{' '}
+            <a
+              href={'http://mnat.herokuapp.com/api/resume'}
+              onClick={() => {
+                ReactGa.event({
+                  category: 'Resume',
+                  action: 'Downloaded resume.',
+                });
+              }}
+            >
+              <FontAwesomeIcon icon={faDownload} />
+            </a>
+          </h1>
+
+          <h4>
+            Contains a list of my <span className="text-success">projects</span>
+            , <span className="text-success">skills</span>,{' '}
+            <span className="text-success">education / courses</span>,{' '}
+            <span className="text-success">awards</span>, and{' '}
+            <span className="text-success">more</span>.
+          </h4>
+        </div>
+        <div
+          className="text-center row justify-content-center"
+          style={{ marginBottom: '2em', marginTop: '2em' }}
+        >
+          <button
+            className="col-xs-2 btn btn-success"
+            disabled={this.state.page === 1}
+            style={{
+              cursor: this.state.page === 1 ? 'not-allowed' : 'default',
+            }}
             onClick={() => {
-              ReactGa.event({
-                category: 'Resume',
-                action: 'Downloaded resume.',
-              });
+              this.setState((state, props) => ({ page: state.page - 1 }));
             }}
           >
-            <FontAwesomeIcon icon={faDownload} />
-          </a>
-        </h1>
-
-        <h4>
-          Contains a list of my <span className="text-success">projects</span>,{' '}
-          <span className="text-success">skills</span>,{' '}
-          <span className="text-success">education / courses</span>,{' '}
-          <span className="text-success">awards</span>, and{' '}
-          <span className="text-success">more</span>.
-        </h4>
-      </div>
-      <div
-        className="text-center row justify-content-center"
-        style={{ marginBottom: '2em', marginTop: '2em' }}
-      >
-        <button
-          className="col-xs-2 btn btn-success"
-          disabled={page === 1}
-          style={{ cursor: page === 1 ? 'not-allowed' : 'default' }}
-          onClick={() => {
-            setPage(page - 1);
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+          <h5
+            className="col-xs-2"
+            style={{ paddingTop: 8, marginLeft: '1em', marginRight: '1em' }}
+          >
+            Page {this.state.page} of {this.state.pages}
+          </h5>
+          <button
+            className="col-xs-2 btn btn-success"
+            disabled={this.state.page === this.state.pages}
+            style={{
+              cursor:
+                this.state.page === this.state.pages
+                  ? 'not-allowed'
+                  : 'default',
+            }}
+            onClick={() => {
+              this.setState((state, props) => ({ page: this.state.page + 1 }));
+            }}
+          >
+            <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        </div>
+        <div
+          style={{
+            marginBottom: '2em',
+            overflow: 'auto',
           }}
+          className={this.state.width > 768 ? 'row justify-content-center' : ''}
         >
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </button>
-        <h5
-          className="col-xs-2"
-          style={{ paddingTop: 8, marginLeft: '1em', marginRight: '1em' }}
-        >
-          Page {page} of {pages}
-        </h5>
-        <button
-          className="col-xs-2 btn btn-success"
-          disabled={page === pages}
-          style={{ cursor: page === pages ? 'not-allowed' : 'default' }}
-          onClick={() => {
-            setPage(page + 1);
-          }}
-        >
-          <FontAwesomeIcon icon={faArrowRight} />
-        </button>
+          <Document
+            file={require('../assets/mahesh-natamai-resume.pdf')}
+            onLoadSuccess={({ _pdfInfo }) =>
+              this.setState({ pages: _pdfInfo.numPages })
+            }
+          >
+            <Page pageNumber={this.state.page} />
+          </Document>
+        </div>
       </div>
-      <div
-        className="row justify-content-center"
-        style={{ marginBottom: '2em' }}
-      >
-        <Document
-          file={require('../assets/mahesh-natamai-resume.pdf')}
-          onLoadSuccess={({ _pdfInfo }) => {
-            console.log(_pdfInfo.numPages);
-            setPages(_pdfInfo.numPages);
-          }}
-        >
-          <Page pageNumber={page} />
-        </Document>
-      </div>
-    </div>
-  );
+    );
+  }
 }
