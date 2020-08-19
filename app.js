0cb0f3aaa3abf04a6c7d700ba33b5175abfda31f
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-08-19 02:26:27 -0500

    debugging forced https redirect

--- app.js
+++ app.js
@@ -39,10 +39,8 @@ app.use('/api/resume', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res, next) => {
-    if (req.protocol === 'http') {
-      res.redirect('https://' + req.headers.host + req.url);
-      next();
-    }
+    if (req.protocol === 'http')
+      return res.redirect('https://' + req.headers.host + req.url);
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
